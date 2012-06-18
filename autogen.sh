321ad879ddbd7b4c36a96354cfec1e6ebe4ea189
Yuriy Syrota <ysyrota@gmail.com>
2012-06-18 22:36:38 +0300

    Use this to gen Makefile and configure

---
+++ autogen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+THEDIR="`pwd`"
+cd "$srcdir"
+DIE=0
+
+#Don't do this unless using libtool
+#libtoolize --force --automake
+aclocal
+autoconf
+autoheader
+automake --foreign --add-missing
+
+if test -z "$*"; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+fi
+
+cd "$THEDIR"
+
+$srcdir/configure "$@"
+
